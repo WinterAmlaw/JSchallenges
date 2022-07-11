a060e063f0d8c70144a2fb960554cc6efc1a82cd
WinterAmlaw <108776489+WinterAmlaw@users.noreply.github.com>
2022-07-11 16:33:42 -0400

    Update Challenges.js

--- Challenges.js
+++ Challenges.js
@@ -430,3 +430,15 @@ function findAncestor(generation, gender) {
 
 console.log(findAncestor(-3, "m"));
 
+// Write a function called displayName() that takes an object as an argument and prints the person's first and last name. Use object destructuring in the function argument. And also, use template strings when printing the first and last name.
+
+function displayName({firstName, lastName}) {
+ return `${firstName} ${lastName}`;
+}
+const me = {
+ firstName: 'Winter',
+ lastName: 'Amlaw',
+ hobbies: ['Programming, Electronics']
+}
+console.log(displayName(me));
+
